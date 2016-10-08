bdaf220b1f7b3c564a70cd9256aa7237028fb36f
Marcelo Carranza <marrselo@gmailcom>
2016-10-08 11:40:17 -0500

    ok todo

---
+++ upload/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://local.test/opencart/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://local.test/opencart/upload/');
+
+// DIR
+define('DIR_APPLICATION', '/home/chelo/Sistemas/PHP/test/opencart/upload/catalog/');
+define('DIR_SYSTEM', '/home/chelo/Sistemas/PHP/test/opencart/upload/system/');
+define('DIR_IMAGE', '/home/chelo/Sistemas/PHP/test/opencart/upload/image/');
+define('DIR_LANGUAGE', '/home/chelo/Sistemas/PHP/test/opencart/upload/catalog/language/');
+define('DIR_TEMPLATE', '/home/chelo/Sistemas/PHP/test/opencart/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/home/chelo/Sistemas/PHP/test/opencart/upload/system/config/');
+define('DIR_CACHE', '/home/chelo/Sistemas/PHP/test/opencart/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/chelo/Sistemas/PHP/test/opencart/upload/system/storage/download/');
+define('DIR_LOGS', '/home/chelo/Sistemas/PHP/test/opencart/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/chelo/Sistemas/PHP/test/opencart/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/home/chelo/Sistemas/PHP/test/opencart/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '123456');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
